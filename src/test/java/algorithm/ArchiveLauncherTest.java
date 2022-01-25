ba72e3eb7f954c01c26ee397b52895bc307f274b
yhh1056 <yhh1056@naver.com>
2022-01-25 16:16:49 +0900

    feat: 프로그래머스 기능 삭제

--- src/test/java/algorithm/ArchiveLauncherTest.java
+++ src/test/java/algorithm/ArchiveLauncherTest.java
@@ -3,10 +3,7 @@ package algorithm;
 import algorithm.archive.GithubConfig;
 import algorithm.archive.provider.baekjoon.Baekjoon;
 import algorithm.archive.provider.baekjoon.BaekjoonLevel;
-import algorithm.archive.provider.programmers.Programmers;
-import algorithm.archive.provider.programmers.ProgrammersLevel;
 import algorithm.file.DefaultBaekjoonMarkdownGenerator;
-import algorithm.file.DefaultProgrammersMarkdownGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +14,6 @@ class ArchiveLauncherTest {
     void generateProvider() {
         GithubConfig config = new GithubConfig("yhh1056", "repo", "main");
         ArchiveLauncher launcher = new ArchiveLauncher();
-        launcher.run(new DefaultProgrammersMarkdownGenerator(config));
         launcher.run(new DefaultBaekjoonMarkdownGenerator(config));
     }
 
@@ -25,6 +21,3 @@ class ArchiveLauncherTest {
 
 @Baekjoon(number = 10001, level = BaekjoonLevel.RUBY)
 class Solve1 {}
-
-@Programmers(name = "test", url = "https://testsetse/10001", level = ProgrammersLevel.LEVEL5, number = 10001)
-class Solve2 {}
